22df7dc3708b3609a7d0e1527af3add07353a422
Ankush Bhardwaj <40923324+ankushbhardwxj@users.noreply.github.com>
2022-12-30 22:41:28 +0530

    Initial Commit

---
+++ constant/constantApp.swift
@@ -0,0 +1,17 @@
+//
+//  constantApp.swift
+//  constant
+//
+//  Created by Ankush Bhardwaj on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct constantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
